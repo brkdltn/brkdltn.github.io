a5a860fe7386c9050b520b68d31e0fb734aa55ae
Brooke <bdalton@ucsc.edu>
2020-11-13 21:40:31 -0800

    And the password is..?

---
+++ stars.js
@@ -0,0 +1,81 @@
+  (function () {
+      var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
+              window.setTimeout(callback, 1000 / 60);
+          };
+      window.requestAnimationFrame = requestAnimationFrame;
+  })();
+
+  // background stuff.
+  var background = document.getElementById("bgCanvas"),
+      bgCtx = background.getContext("2d"),
+      width = window.innerWidth,
+      height = document.body.offsetHeight;
+
+  (height < 400) ? height = 400 : height;
+
+  background.width = width;
+  background.height = height;
+
+  // canvas used for the stars
+  bgCtx.fillStyle = '#05004c';
+  bgCtx.fillRect(0, 0, width, height);
+
+  // stars
+  function Star(options) {
+      this.size = Math.random() * 5;
+      this.speed = Math.random() * .05;
+      this.x = options.x;
+      this.y = options.y;
+  }
+
+  Star.prototype.reset = function () {
+      this.size = Math.random() * 5;
+      this.speed = Math.random() * .05;
+      this.x = width;
+      this.y = Math.random() * height;
+  }
+
+  Star.prototype.update = function () {
+      this.x += this.speed;
+      if (this.x < 0) {
+          this.reset();
+      } else {
+          bgCtx.fillRect(this.x, this.y, this.size, this.size);
+      }
+  }
+
+  var entities = [];
+
+  // initialize the stars
+  for (var i = 0; i < height; i++) {
+      entities.push(new Star({
+          x: Math.random() * width,
+          y: Math.random() * height
+      }));
+  }
+
+  // animate background
+  function animate() {
+	  bgCtx.fillStyle = '#000';
+      bgCtx.fillRect(0, 0, width, height);
+      bgCtx.fillStyle = '#ffffff';
+      bgCtx.strokeStyle = '#ffffff';
+
+      var entLen = entities.length;
+
+      while (entLen--) {
+          entities[entLen].update();
+      }
+      requestAnimationFrame(animate);
+  }
+  animate();
+
+  // astronaut animation
+    $(document).ready(function(e) {
+        var width = "+=" + $(document).width();
+        $("#astroanimate").animate({
+        left: width
+      }, 5000, function() {
+        $("#astroanimate").css("display", "none");
+      });
+    });
